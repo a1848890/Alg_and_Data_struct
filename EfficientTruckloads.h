99b11eefa6d423aeeac87705bdb2ae064725a0f9
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-08-25 19:30:07 +0930

    Add files via upload

---
+++ EfficientTruckloads.h
@@ -0,0 +1,10 @@
+#include<vector>
+#pragma once
+
+using std::vector;
+
+class EfficientTruckloads{
+    public:
+        EfficientTruckloads();
+        int mem_truckloads(int numCrates, int loadSize);
+};

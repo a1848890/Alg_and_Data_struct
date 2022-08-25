3e96e6bf97257d1debc37d64ddca22ec9f8dfc49
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-08-25 18:46:04 +0930

    Update Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -4,7 +4,8 @@ Truckloads :: Truckloads(){}
 
 
 int Truckloads :: numTrucks(int numCrates, int loadSize){
-    // Base case: 
+        if (numCrates<0 || loadSize<0) return -1;
+	// Base case: 
         if (loadSize >= numCrates) return 1;
 		return numTrucks(numCrates/2 + numCrates%2, loadSize) + numTrucks(numCrates/2, loadSize);
 //    if (numCrates % 2 == 0) return numTrucks(numCrates, loadSize) + numTrucks(numCrates, loadSize);

ae20f6590453b063add912b7662be21857ad6f73
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-09-02 10:33:42 +0930

    Update ReduceGCD.cpp

--- ReduceGCD.cpp
+++ ReduceGCD.cpp
@@ -10,6 +10,18 @@ ReduceGCD :: ReduceGCD(){}
 
 
 int ReduceGCD :: binaryOperator(int x, int y){
-      return std::__algo_gcd(x, y);
+    int _gcd = 0;
+    if ( y > x) {   
+    int temp = y;
+    y = x;
+    x = temp;
+  }
+    
+  for (int i = 1; i <=  y; ++i) {
+    if (x % i == 0 && y % i ==0) {
+      _gcd = i;
+    }
+  }
+      return _gcd;
 }
 

05bb3c777900a2af43098f3d9ea1f68920afc50a
a1848890 <100540904+a1848890@users.noreply.github.com>
2022-10-23 10:43:01 +1030

    Update Polish.cpp

--- Polish.cpp
+++ Polish.cpp
@@ -2,46 +2,13 @@
 
 using namespace std;
 
-Polish::Polish(){
-
-}
-
-// stack<string> Polish::left(string expression){
-//     stack<string> stack;
-//     int length = expression.size();
-
-//     for (int i = length; i >= 0; i--){
-//         string str = "";
-//         while (i >= 0 && expression[i] != ' '){
-//             str = expression[i] + str;
-//             i--;
-//         }
-//         stack.push(str);
-//     }
-//     return stack;
-// }
-
-// stack<long> Polish::right(string expression){
-//     stack<long> stack;
-//     int length = expression.size();
-
-//     for (int i = length; i >= 0; i--){
-//         string str = "";
-//         while (i >= 0 && expression[i] != ' '){
-//             str = expression[i] + str;
-//             i--;
-//         }
-//         stack.push(atol(str.c_str()));
-//     }
-//     return stack;
-// }
+Polish::Polish(){}
 
 string Polish::convert(string expression){
     stack<string> strStack;
     stack<long> calStack;
     int count = 0;
     int counter = 0;
-
     int length = expression.size();
     string c;
 
@@ -54,15 +21,10 @@ string Polish::convert(string expression){
         
         strStack.push(c);
         calStack.push(atol(c.c_str()));
-
-        // strStack = left(expression);
-        // calStack = right(expression);
-
         if (c == "+" || c == "-" || c == "*" || c == "/"){
             while(strStack.size() < 2){
                 return "Error";
             }
-
             counter++;
             if (!strStack.empty()){
                 strStack.pop();
@@ -123,20 +85,11 @@ string Polish::convert(string expression){
             
             strStack.push(temp);
         }
-        
-        
     }
-    // cout << "count: " << count << endl;
-    // cout << "counter: " << counter << endl;
     if (count - counter == counter){
         return strStack.top() + " " + "=" + " " + to_string(calStack.top());
     }else{
         return "Error";
     }
-    
-    
 }
-
-Polish::~Polish(){
-    
-}
+Polish::~Polish(){}
